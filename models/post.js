cd15aba16fd8d35e758cd8b7535ec768c9380b7c
naiiiden <na4ko359@gmail.com>
2023-11-25 14:57:58 +0200

    post model add title

--- models/post.js
+++ models/post.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const Post = mongoose.model(
     "Post",
     new Schema(
-        {
+        {   
+            title: { type: String, required: true },
             text: { type: String, required: true },
             author: { type: String, required: true },
             published: { type: Boolean, required: true },
