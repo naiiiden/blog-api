4464414d6feaf41c2e8f9c28d95bb86f40e53a28
naiiiden <na4ko359@gmail.com>
2023-11-26 14:14:55 +0200

    http methods

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
-const routes = require('./routes/index');
 
-app.use('/user', routes.user);
-app.use('/post', routes.post);
-
-app.get('/', (req, res) => {
-    res.send('hi');
+app.get('/users', (req, res) => {
+    return res.send('GET HTTP method on user resource');
+});
+  
+app.post('/users', (req, res) => {
+    return res.send('POST HTTP method on user resource');
+});
+  
+app.put('/users', (req, res) => {
+    return res.send('PUT HTTP method on user resource');
+});
+  
+app.delete('/users', (req, res) => {
+    return res.send('DELETE HTTP method on user resource');
 });
 
 app.listen(3000, () => {
