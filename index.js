9d35e7269003e05544e2bff18e46756e3e387188
naiiiden <na4ko359@gmail.com>
2023-11-26 17:37:22 +0200

    posts/users mockup data HTTP methods

--- index.js
+++ index.js
@@ -2,11 +2,41 @@ const express = require('express');
 const app = express();
 require('dotenv').config()
 
+let users = {
+    1: {
+      id: '1',
+      username: 'Robin Wieruch',
+    },
+    2: {
+      id: '2',
+      username: 'Dave Davids',
+    },
+  };
+  
+let posts = {
+    1: {
+      id: '1',
+      title: 'First post',
+      text: 'Hello World',
+      userId: '1',
+    },
+    2: {
+      id: '2',
+      title: 'Second post',
+      text: 'By World',
+      userId: '2',
+    },
+};
+
 app.get('/users', (req, res) => {
-    return res.send('GET HTTP method on user resource');
+    return res.send(Object.values(users));
 });
   
-app.post('/users', (req, res) => {
+app.get('/users/:userId', (req, res) => {
+    return res.send(users[req.params.userId]);
+});
+
+app.post('/users/:userId', (req, res) => {
     return res.send('POST HTTP method on user resource');
 });
   
@@ -18,6 +48,14 @@ app.delete('/users/:userId', (req, res) => {
     return res.send(`DELETE HTTP method on user/${req.params.userId} resource`);
 });
 
+app.get('/posts', (req, res) => {
+    return res.send(Object.values(posts));
+  });
+  
+app.get('/posts/:postId', (req, res) => {
+    return res.send(posts[req.params.postId]);
+});
+
 app.listen(process.env.PORT, () => {
-    console.log(`App listening on port 3000`);
+    console.log(`App listening on port ${process.env.PORT}`);
 });
