528fe50702bec3d3fcce8de5532a022394cbc6ec
naiiiden <na4ko359@gmail.com>
2023-11-27 23:46:45 +0200

    blog controllers

--- controllers/blog.js
+++ controllers/blog.js
@@ -6,4 +6,37 @@ blogRouter.get('/', async (req, res) => {
     res.json(blogs);
 });
 
+blogRouter.post('/', async (req, res) => {
+    const { title, body, author, published } = req.body;
+
+    const blog = new Blog({ title, body, author, published });
+
+    const savedBlog = await blog.save();
+    res.json(savedBlog);
+});
+  
+blogRouter.get('/:blogId', async (req, res) => {
+    const blog = await Blog.findById(req.params.blogId);
+    return blog 
+        ? res.json(blog)
+        : res.status(404).end();
+});
+
+blogRouter.put('/:blogId', (req, res) => {
+    const { title, body, author, published, comments } = req.body;
+
+    const blog = { title, body, author, published, comments };
+
+    Blog.findByIdAndUpdate(req.params.blogId, blog, { new: true })
+        .then(updatedBlog => {
+            res.json(updatedBlog)
+        })
+        .catch(error => next(error))
+});
+
+blogRouter.delete('/:blogId', async (req, res) => {
+    await Blog.findByIdAndDelete(req.params.blogId);
+    res.status(204).end();
+});
+
 module.exports = blogRouter;
--- index.js
+++ index.js
@@ -54,44 +54,6 @@ app.delete('/users/:userId', async (req, res) => {
 
 app.use('/blogs', blogRouter);
 
-// app.get('/blogs', async (req, res) => {
-//     const blogs = await Blog.find({});
-//     res.json(blogs);
-// });
-
-// app.post('/blogs', async (req, res) => {
-//     const { title, body, author, published } = req.body;
-
-//     const blog = new Blog({ title, body, author, published });
-
-//     const savedBlog = await blog.save();
-//     res.json(savedBlog);
-// });
-  
-// app.get('/blogs/:blogId', async (req, res) => {
-//     const blog = await Blog.findById(req.params.blogId);
-//     return blog 
-//         ? res.json(blog)
-//         : res.status(404).end();
-// });
-
-// app.put('/blogs/:blogId', (req, res) => {
-//     const { title, body, author, published, comments } = req.body;
-
-//     const blog = { title, body, author, published, comments };
-
-//     Blog.findByIdAndUpdate(req.params.blogId, blog, { new: true })
-//         .then(updatedBlog => {
-//             res.json(updatedBlog)
-//         })
-//         .catch(error => next(error))
-// });
-
-// app.delete('/blogs/:blogId', async (req, res) => {
-//     await Blog.findByIdAndDelete(req.params.blogId);
-//     res.status(204).end();
-// });
-
 app.listen(process.env.PORT, () => {
     console.log(`App listening on port ${process.env.PORT}`);
 });
