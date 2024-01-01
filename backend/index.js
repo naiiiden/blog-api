8e74f678d21c14996429bd09d95b3d2ae9918337
naiiiden <na4ko359@gmail.com>
2024-01-01 10:01:50 +0200

    requestLogger middleware

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const config = require("./utils/config");
+const middleware = require("./utils/middleware");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,6 +16,7 @@ const userRouter = require("./controllers/user");
 const blogRouter = require("./controllers/blog");
 const loginRouter = require("./controllers/login");
 
+app.use(middleware.requestLogger);
 app.use("/users", userRouter);
 app.use("/blogs", blogRouter);
 app.use("/login", loginRouter);
