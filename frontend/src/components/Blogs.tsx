da020282f83002079779d071a422aa668bbfb200
naiiiden <na4ko359@gmail.com>
2024-01-03 18:34:23 +0200

    render all published blogs

--- frontend/src/components/Blogs.tsx
+++ frontend/src/components/Blogs.tsx
@@ -15,6 +15,32 @@ const Blogs = () => {
   return (
     <div>
       <p>All published blogs will be rendered here:</p>
+      <ul
+        style={{
+          padding: "0",
+          listStyle: "none",
+          display: "flex",
+          flexWrap: "wrap",
+          gap: "1rem",
+        }}
+      >
+        {blogs.map((blog) => {
+          return (
+            blog.published === true && (
+              <li
+                style={{
+                  border: "1px solid red",
+                  padding: "1rem",
+                }}
+              >
+                <a href={blog.title}>{blog.title}</a>
+                <p>{blog.createdAt}</p>
+                <p>{blog.comments.length} comments</p>
+              </li>
+            )
+          );
+        })}
+      </ul>
     </div>
   );
 };
