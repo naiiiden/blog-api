66ff85b96b9dd5839b001cd386c428cce3b81a5c
naiiiden <na4ko359@gmail.com>
2024-01-04 09:04:54 +0200

    install react-router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import Blogs from "./components/Blogs";
 import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <>
       <Header />
       <main>
-        <Blogs />
+        <Routes>
+          <Route path="/" element={<Blogs />} />
+        </Routes>
       </main>
     </>
   );
