6f38ac373a0468ccd22804e930f9318bf880d061
naiiiden <na4ko359@gmail.com>
2024-01-07 17:49:00 +0200

    all-blogs page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Login from "./components/Login";
 import { Routes, Route } from "react-router-dom";
 import NewBlog from "./components/NewBlog";
+import AdminBlogs from "./components/AdminBlogs";
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
           <Route path="/:blogId" element={<Blog />} />
           <Route path="/login" element={<Login />} />
           <Route path="/new-blog" element={<NewBlog />} />
+          <Route path="/all-blogs" element={<AdminBlogs />} />
         </Routes>
       </main>
     </>
---
+++ frontend/src/components/AdminBlogs.tsx
@@ -0,0 +1,57 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { BlogType } from "../interfaces";
+import { Link } from "react-router-dom";
+
+const AdminBlogs = () => {
+  const [blogs, setBlogs] = useState<BlogType[]>([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/blogs").then((res) => {
+      setBlogs(res.data);
+    });
+  }, []);
+
+  console.log(blogs);
+
+  return (
+    <div>
+      <h1>admin page for blogs to edit/publish or unpublish</h1>
+      <p>All blogs will be rendered here:</p>
+      <ul
+        style={{
+          padding: "0",
+          listStyle: "none",
+          display: "flex",
+          flexWrap: "wrap",
+          gap: "1rem",
+        }}
+      >
+        {blogs.map((blog) => {
+          return (
+            <li
+              key={blog._id}
+              style={{
+                border: "1px solid red",
+                padding: "1rem",
+              }}
+            >
+              <Link to={blog._id}>{blog.title}</Link>
+              <p>{blog.comments?.length} comments</p>
+              <p>
+                created:{" "}
+                {new Date(blog.createdAt).toLocaleDateString(undefined, {
+                  month: "short",
+                  day: "numeric",
+                  year: "numeric",
+                })}
+              </p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default AdminBlogs;
