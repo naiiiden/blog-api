4a404b8e21885af460dec934336cf850a3ca5763
naiiiden <na4ko359@gmail.com>
2024-01-08 22:00:08 +0200

    update blog form setup

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -61,6 +61,9 @@ const Blog = () => {
 
   console.log(1, newComment);
 
+  const [updateBlog, setUpdateBlog] = useState(false);
+  console.log(updateBlog);
+
   return (
     <div>
       <h1>{blog.title}</h1>
@@ -124,6 +127,55 @@ const Blog = () => {
         ></textarea>
         <input type="submit" value="submit" />
       </form>
+
+      <button onClick={() => setUpdateBlog(!updateBlog)}>update blog</button>
+      {updateBlog && (
+        <form
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "start",
+          }}
+        >
+          <label htmlFor="title">title:</label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            value={blog.title}
+            // onChange={(e) => setNewBlog({ ...newBlog, title: e.target.value })}
+          />
+          <label htmlFor="body">body:</label>
+          <textarea
+            name="body"
+            id="body"
+            cols={30}
+            rows={10}
+            value={blog.body}
+            // onChange={(e) => setNewBlog({ ...newBlog, body: e.target.value })}
+          ></textarea>
+          <fieldset>
+            <legend>published:</legend>
+            <label htmlFor="yes">yes:</label>
+            <input
+              type="radio"
+              name="published"
+              id="yes"
+              // onChange={() => setNewBlog({ ...newBlog, published: true })}
+              checked={blog.published === true}
+            />
+            <label htmlFor="no">no:</label>
+            <input
+              type="radio"
+              name="published"
+              id="no"
+              // onChange={() => setNewBlog({ ...newBlog, published: false })}
+              checked={blog.published === false}
+            />
+          </fieldset>
+          <input type="submit" value="submit" />
+        </form>
+      )}
     </div>
   );
 };
