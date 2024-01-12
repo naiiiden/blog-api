02ef6c18382f4353653f10f3c64746760f78d79b
naiiiden <na4ko359@gmail.com>
2024-01-12 23:58:12 +0200

    userRouter improvements

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,42 +3,64 @@ const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 
 userRouter.get("/", async (req, res) => {
-  const users = await User.find({}).populate('blogs', { title: 1, published: 1 });
-  res.json(users);
+  try {
+    const users = await User.find({}).populate("blogs", {
+      title: 1,
+      published: 1,
+    });
+    res.json(users);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 userRouter.post("/", async (req, res) => {
-  const { username, password } = req.body;
+  try {
+    const { username, password } = req.body;
 
-  const saltRounds = 10;
-  const passwordHash = await bcrypt.hash(password, saltRounds);
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds);
 
-  const user = new User({ username, passwordHash });
+    const user = new User({ username, passwordHash });
 
-  const savedUser = await user.save();
-  res.json(savedUser);
+    const savedUser = await user.save();
+    res.json(savedUser);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 userRouter.get("/:userId", async (req, res) => {
-  const user = await User.findById(req.params.userId);
-  return user ? res.json(user) : res.status(404).end();
+  try {
+    const user = await User.findById(req.params.userId);
+    return user ? res.json(user) : res.status(404).end();
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-userRouter.put("/:userId", (req, res) => {
-  const { username, password } = req.body;
+userRouter.put("/:userId", async (req, res) => {
+  try {
+    const { username, password } = req.body;
 
-  const user = { username, password };
+    const user = { username, password };
 
-  User.findByIdAndUpdate(req.params.userId, user, { new: true })
-    .then((updatedUser) => {
-      res.json(updatedUser);
-    })
-    .catch((error) => next(error));
+    const updatedUser = await User.findByIdAndUpdate(req.params.userId, user, {
+      new: true,
+    });
+    res.json(updatedUser);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 userRouter.delete("/:userId", async (req, res) => {
-  await User.findByIdAndDelete(req.params.userId);
-  res.status(204).end();
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(204).end();
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = userRouter;
